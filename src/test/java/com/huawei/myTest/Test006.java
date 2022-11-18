d1912cd9959bba9f149d2d6de252e40f2763a1b0
szx <455587904@qq.com>
2022-11-18 15:21:00 +0800

    Test006

---
+++ src/test/java/com/huawei/myTest/Test006.java
@@ -0,0 +1,50 @@
+package com.huawei.myTest;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.huawei.domain.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
+public class Test006 {
+    public static void main(String[] args) {
+        String completeResult = "{\"msg\":\"操作成功\",\"code\":200,\"data\":{\"finishFlag\":true,\"backFlag\":false,\"varMap\":{}}}";
+//        System.out.println(completeResult);
+        JSONObject jsonObject = JSON.parseObject(completeResult);
+        String data = MapUtils.getString(jsonObject, Result.DATA_TAG, "");
+//        System.out.println(data);
+        Map<String, Object> map1 = JSON.parseObject(data);
+//        System.out.println(map1);
+
+
+        jsonToMap(completeResult);
+
+        String s1 = "{\"msg\":\"操作成功\",\"code\":200}";
+        Map<String, Object> s2 = jsonToMap(s1);
+        System.out.println(s2);
+        if(Objects.isNull(s2.get("code"))){
+            System.out.println("====为空====");
+        }
+        Object code = s2.get("code");
+        String code1 = MapUtils.getString(s2, "code");
+        System.out.println(code1);
+    }
+
+    private static Map<String, Object> jsonToMap(String s) {
+        JSONObject jsonMap = null;
+        try {
+            jsonMap = JSON.parseObject(s);
+        } catch (Exception e) {
+            log.error("{}", e);
+        }
+        return jsonMap;
+    }
+
+
+
+
+}
